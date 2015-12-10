efc3bfdf678d361095003208fde2ebe39a7c3c5b
cesare rocchetti <cesare@cr-consult.eu>
2015-12-10 11:17:51 +0100

    Comincio con model mail send

---
+++ CRConsultMvc/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRConsultMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Networking()
+        {
+            ViewBag.Message = "Networking";
+            return View();
+        }
+        public ActionResult WebPage()
+        {
+            ViewBag.Message = "Pagina Web";
+            return View();
+        }
+        public ActionResult Assistenza()
+        {
+            ViewBag.Message = "Assistenza";
+            return View();
+        }
+        public ActionResult Privacy()
+        {
+            ViewBag.Message = "Documento privacy";
+            return View();
+        }
+    }
+}
---
+++ CRConsultMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CRConsultMvc.MvcApplication" Language="C#" %>
