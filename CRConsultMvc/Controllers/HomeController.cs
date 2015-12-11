39832e37c5eadc039647696e7ac081cff9e7a109
cesare rocchetti <cesare@cr-consult.eu>
2015-12-11 08:36:55 +0100

    Finito le tre parti principali Networking WebbApp Assistenza

--- CRConsultMvc/Controllers/HomeController.cs
+++ CRConsultMvc/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CRConsultMvc.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 
 namespace CRConsultMvc.Controllers
 {
@@ -13,6 +17,29 @@ namespace CRConsultMvc.Controllers
             return View();
         }
 
+        //index.post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Index(EmailFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<p><strong>Email da:</strong> {0} ({1})</p><p><strong>Ditta: </strong>{2}</p><p><strong>Telefono:</strong> {3}</p><p><strong>Message:</strong></p><p>{4}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("cesare@cr-consult.eu"));
+                message.From = new MailAddress("webservice@cr-consult.it");
+                message.Subject = "Richiesta info inviata dal sito CRConsult.eu";
+                message.Body = string.Format(body, model.Nome, model.Email, model.Ditta, model.Telefono, model.Messaggio);
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient())
+                {
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("MailInviata");
+                }
+            }
+            return View(model);
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -44,6 +71,10 @@ namespace CRConsultMvc.Controllers
         public ActionResult Privacy()
         {
             ViewBag.Message = "Documento privacy";
+            return View();
+        }
+        public ActionResult MailInviata()
+        {
             return View();
         }
     }
---
+++ CRConsultMvc/Models/EmailFormModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRConsultMvc.Models
+{
+    public class EmailFormModel
+    {
+        [Required, Display(Name = "Nome")]
+        public string Nome { get; set; }
+        public string Ditta { get; set; }
+        [Required, Display(Name ="Telefono")]
+        public string Telefono { get; set; }
+        [Required, Display(Name = "Email"), EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Messaggio { get; set; }
+        [Required, Display(Name ="Privacy")]
+        public bool Privacy { get; set; }
+    }
+}
