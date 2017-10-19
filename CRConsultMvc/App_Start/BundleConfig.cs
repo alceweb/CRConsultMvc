185b4f158e8360d852ac4315f3c73df77fb8a292
cesare rocchetti <cesare@cr-consult.eu>
2017-10-19 08:48:37 +0200

    Colore di sfondo

--- CRConsultMvc/App_Start/BundleConfig.cs
+++ CRConsultMvc/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace CRConsultMvc
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.css",
                       "~/Content/overlay-bootstrap.css",
                       "~/Content/site.css"));
         }
